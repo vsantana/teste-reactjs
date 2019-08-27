6cbc66fb84ed383ee61987f1fc48f9556ed54c48
Valmir Santana <vsantana@gmail.com>
2019-08-27 10:29:04 -0300

    Init

---
+++ precache-manifest.03383cdd2053ec65b875e326d4a4f226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93448fa0789aba0e9ecef4006e00cc5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46a7d46db9117c70c501",
+    "url": "./static/css/main.066b2c2f.chunk.css"
+  },
+  {
+    "revision": "a6876cfe1df3b47c0d2c",
+    "url": "./static/js/2.d828d6ca.chunk.js"
+  },
+  {
+    "revision": "46a7d46db9117c70c501",
+    "url": "./static/js/main.e9c48acb.chunk.js"
+  },
+  {
+    "revision": "18a2e58ed7f8a6509bfc",
+    "url": "./static/js/runtime~main.de823a4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ precache-manifest.ff086dba0f8f016de0ad029dd27453c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30467306250e17c3808bfa30dd92a6f",
+    "url": "teste-reactjs/index.html"
+  },
+  {
+    "revision": "7c23c4f3098f123f762d",
+    "url": "teste-reactjs/static/css/main.066b2c2f.chunk.css"
+  },
+  {
+    "revision": "a6876cfe1df3b47c0d2c",
+    "url": "teste-reactjs/static/js/2.d828d6ca.chunk.js"
+  },
+  {
+    "revision": "7c23c4f3098f123f762d",
+    "url": "teste-reactjs/static/js/main.d16f815a.chunk.js"
+  },
+  {
+    "revision": "9abbd89ae6831559d4ac",
+    "url": "teste-reactjs/static/js/runtime~main.c9a3fc38.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "teste-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
